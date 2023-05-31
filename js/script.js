e8d122962a883c730fd3d9a4d34cdeb7dbab18b6
analebyodkina <dp030992cad@gmail.com>
2023-05-31 18:57:31 +0300

    added cube

--- js/script.js
+++ js/script.js
@@ -111,3 +111,26 @@ function draw(){
   ctx.restore();
   requestAnimationFrame(draw);
 }
+
+
+// carousel reflections section
+$('.owl-carousel').owlCarousel({    
+    lazyLoad:true,
+    loop:true,
+    margin:24,
+    autoHeight: true,
+    nav: true,      
+    responsive : {
+      // breakpoint from 0 up
+      0 : {
+          items:1.3,          
+      },      
+      576 : {
+          items:2,                 
+      },
+      // breakpoint from 992 up      
+      768 : {
+        items:3,        
+      }
+    }
+});
