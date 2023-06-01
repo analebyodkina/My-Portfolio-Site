d561ce4fa63a98872f7d16efb827f3508999b967
analebyodkina <dp030992cad@gmail.com>
2023-06-01 12:41:13 +0300

    added contact button to header

--- js/script.js
+++ js/script.js
@@ -21,3 +21,15 @@ $('.owl-carousel').owlCarousel({
       }
     }
 });
+
+
+// Force a hover to see the effect
+const share = document.querySelector('.share');
+
+setTimeout(() => {
+  share.classList.add("hover");
+}, 1000);
+
+setTimeout(() => {
+  share.classList.remove("hover");
+}, 3000);
